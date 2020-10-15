1593769a2b9f80777e10ce71c9b7af19fd3f8e08
Mohamed Edamer <edamer.mo@gmail.com>
2020-10-15 16:11:24 -0700

    fixed the bug

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,16 +5,17 @@ from util import Queue, Stack
 import random
 from ast import literal_eval
 
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "./maps/test_line.txt"
-map_file = "./maps/test_cross.txt"
+# map_file = "./maps/test_cross.txt"
 # map_file = "./maps/test_loop.txt"
 # map_file = "./maps/test_loop_fork.txt"
-# map_file = "./maps/main_maze.txt"
+map_file = "./maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,94 +26,91 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-my_graph = {}
 
-for k, v in room_graph.items():
-    my_graph[k] = v[1]
-# cur = world.starting_room
-# print(cur) # print the starting room
-# print(cur.get_exits()) # get the exits of the room
-# d = cur.get_exits()
-# print(cur.get_room_in_direction(d[0])) # go to the next room
-# cur = cur.get_room_in_direction(d[0]) # set the new room as current
-# print(cur.get_exits())
-#################################
+visited = set()
 
-# def build_graph(old_graph):
-#     new_graph = {}
-#     for k, v in old_graph.items():
-#         new_graph[k] = v[1]
-#     return new_graph
+my_graph = {player.current_room.id: {d: '?' for d in player.current_room.get_exits()}}
 
-# print(build_graph(room_graph))
-# new_graph = build_graph(room_graph)
-# print(len(new_graph))
+opzt = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
 
-# cur_room = new_graph
-# print(cur_room)
-######################
-# def get_path(cur_room):
-#     q = Queue()
-#     q.enqueue([cur_room])
-#     # q = Stack()
-#     # q.push([cur_room])
-#     opz = {'n':'s', 'e':'w', 's':'n', 'w':'e'}
-#     while q.size() > 0:
-#         path = q.dequeue()
-#         # path = q.pop()
-#         v = path[-1]
-#         #breakpoint()
-#         if len(path) < len(my_graph):
-#             l = []
-#             for k, v in my_graph.items():
-#                 for direction in v:
-#                     if not v[direction]:
-#                         direction = opz[direction]
-#                     if v[direction]:
-#                         new_path = list(path)
-#                         new_path.append(int(v[direction])) 
-#                         traversal_path.append(direction)
-#                         #breakpoint()
-#                         q.enqueue(new_path)
-                    
-#         return traversal_path
+def available_exit(room_id):
+    unvisited_direction = []
 
-##################################
-def get_path(cur_room):
+    for d in my_graph[room_id]:
+        if my_graph[room_id][d] == '?':
+            unvisited_direction.append(d)
+    return unvisited_direction
+
+def travel(room_id):
+    while len(available_exit(room_id)) > 0:
+        direction = random.choice(available_exit(room_id))
+        prev = player.current_room.id
+        player.travel(direction)
+        traversal_path.append(direction)
+        if player.current_room.id not in my_graph:
+            my_graph[player.current_room.id] = {d: '?' for d in player.current_room.get_exits()}
+        my_graph[player.current_room.id][opzt[direction]] = prev
+        my_graph[prev][direction] = player.current_room.id
+        room_id = player.current_room.id
+
+def find_target(room_id):
     q = Queue()
-    q.enqueue([cur_room])
-    visited = {}
-    opz = {'n':'s', 'e':'w', 's':'n', 'w':'e'} # go back
+    q.enqueue(room_id)
+    visited = set()
+
+    while q.size() > 0:
+        room = q.dequeue()
+
+        if room not in visited:
+            visited.add(room)
+
+            if len(available_exit(room)) > 0:
+                return room
+            
+            for next_room in list(my_graph[room].values()):
+                q.enqueue(next_room)
+
+def travel_back(target_room, starting_room):
+    q = Queue()
+    q.enqueue([starting_room])
+    visited = set()
+    final_path = []
+
     while q.size() > 0:
         path = q.dequeue()
-        v = path[-1]
-        if len(path) < len(room_graph):
-            for door in player.current_room.get_exits():
-                if not int(player.current_room.get_room_in_direction(door).name[5:]) and v in visited:
-                    door = opz[door]
-                    traversal_path.append(door)
-                elif int(player.current_room.get_room_in_direction(door).name[5:]) != v:
-                    traversal_path.append(door)
-                    #visited[v] = path
-                    new_path = list(path)
-                    new_path.append(int(player.current_room.get_room_in_direction(door).name[5:]))
-                    q.enqueue(new_path)
-                # else:
-                #     del traversal_path[-1]
-                #     door = opz[door]
-                #     new_path = list(path)
-                #     traversal_path.append(door)
-                #     new_path.append(int(player.current_room.get_room_in_direction(door).name[5:]))
-                #     q.enqueue(new_path)
-                
-    return traversal_path
-###################################   
-traversal_path= get_path(player.current_room.id)
-print(traversal_path)
+        room = path[-1]
+        
+        if room not in visited:
+            visited.add(room)
+
+            if room == target_room:
+                final_path = path
+                break
+
+            for next_room in list(my_graph[room].values()):
+                new_path = path.copy() + [next_room]
+
+                q.enqueue(new_path)
+    
+    final_direction = []
+    for i in range(len(final_path) - 1):
+        for direction in my_graph[final_path[i]]:
+            if my_graph[final_path[i]][direction] == final_path[i + 1]:
+                final_direction.append(direction)
+
+    return final_direction
+
+
+while len(my_graph) < len(room_graph):
+    travel(player.current_room.id)
+    target = find_target(player.current_room.id)
+    path = travel_back(target, player.current_room.id)
+    for d in path:
+        player.travel(d)
+        traversal_path.append(d)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -129,7 +127,7 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-print(len(traversal_path))
+
 
 #######
 # UNCOMMENT TO WALK AROUND for manual use:
